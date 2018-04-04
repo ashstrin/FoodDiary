71c3947b92a5a2378733488ab8e745f67c150c32
ashleystringer <ashstrin@umail.iu.edu>
2018-04-04 01:38:41 -0400

    First commit? I think?

--- index.php
+++ index.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-       //include "navigation/header.php";
-        
-        ?>
-    </body>
-</html>
+<?php
+require_once("application/config.php");
+require_once("application/autoloader.class.php");
+
+echo "Guess what?";
+
+new Dispatcher();
