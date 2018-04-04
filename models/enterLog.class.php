b2e953dcb6ad1a58381ebc0189e9eaf52e18af48
ashleystringer <ashstrin@umail.iu.edu>
2018-04-04 01:38:45 -0400

    First commit? I think?

--- models/enterLog.class.php
+++ models/enterLog.class.php
@@ -1,2 +1,22 @@
 <?php
-class EnterLog{}
+class EnterLog{
+    private $id, $content, $date, $name;
+    public function __construct($id, $content, $date, $name){
+        $this->id = $id;
+        $this->content = $content;
+        $this->date = $date;
+        $this->name = $name;
+    }
+    public function getID(){
+        return $this->id;
+    }
+    public function getContent(){
+        return $this->content;
+    }
+    public function getDate(){
+        return $this->date;
+    }
+    public function getName(){
+        return $this->name;
+    }
+}
