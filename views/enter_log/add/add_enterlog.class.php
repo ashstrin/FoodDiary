dcebe8bf12e8fcec43af7f6a62d5b64a47641695
ashleystringer <ashstrin@umail.iu.edu>
2018-04-04 01:37:34 -0400

    First commit? I think?

---
+++ views/enter_log/add/add_enterlog.class.php
@@ -0,0 +1,13 @@
+<?php
+class EnterLogAdd extends IndexView{
+    public function display(){
+        parent::displayHeader("Add");
+        ?>
+<form action="POST" method="">
+    <input type="input" name="">
+    <input type="" name="">
+    <textarea name=""></textarea>
+</form>
+<?php
+    }
+}
