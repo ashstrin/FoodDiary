4974978eb6e9b7227384eb624d278f139fe897fa
ashleystringer <ashstrin@umail.iu.edu>
2018-04-04 01:38:16 -0400

    First commit? I think?

--- models/enterLog_model.class.php
+++ models/enterLog_model.class.php
@@ -3,9 +3,18 @@ class EnterLogModel{
     private $db;
     private $db_connection;
     private $_instance = NULL;
+    private $tblFoodLog;
     
     public function __construct(){
         $this->db = Database::getDatabase();
         $this->db_connection = Database::getConnection();
+        $this->tblFoodLog = Database::getTblFoodLog();
     }
+    
+    public function add(){
+        $query = "INSERT INTO " . $this->tblFoodLog . " (`content`, `date`, `name`)"
+                . "VALUES($content, $date, $name)";
+        $this->db_connection->query($query);
+    }
+    
 }
