8b034367aaeb1422d86daff8bf5c79c0767825bf
ashleystringer <ashstrin@umail.iu.edu>
2018-04-04 01:37:49 -0400

    First commit? I think?

---
+++ www/js/ajax_autosuggestion.js
@@ -0,0 +1,10 @@
+var xmlHttp;
+var numTitles = 0;
+var activeTitle = -1;
+var searchBoxObj, suggestionBoxObj;
+function createXmlHttpObject(){
+    
+}
+window.onload = function(){};
+window.onclick = function(){};
+
