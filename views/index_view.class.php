e3401f1dc136746354cc5a8d745d09a633728270
ashleystringer <ashstrin@umail.iu.edu>
2018-04-04 01:38:21 -0400

    First commit? I think?

--- views/index_view.class.php
+++ views/index_view.class.php
@@ -1,19 +1,49 @@
 <?php
 
 class IndexView{
-    public function displayHeader(){
+    public function displayHeader($title){
         ?>
         <!DOCTYPE html> 
         <html>
             <head>
-                <link>
+                <title><?php echo $title; ?></title>
+                <meta>
+                <!--<link rel="stylesheet" type="text/css" href="../www/css/style.css">-->
+                <style>
+                    #navbar{
+                        width: 65%;
+                        height: 50px;
+                        border: 1px solid gray;
+                        background-color: #ccc;
+                    } 
+                    #navbar div{
+                        position: relative;
+                        display: inline;
+                        top: 10px;
+                        padding: 25px;
+                    }
+                    #navbar div a{
+                        text-decoration: none;
+                        padding: 5px;
+                   
+                        
+                    }
+                    #navbar div a:hover{
+                        
+                    }
+                </style>
             </head>
         <body>
-            <div>
-                <div><a href=""></a></div>
+            <div id="navbar">
+                <div><a href="<?=BASE_URL?>/displaylog/"></a></div>
                 <div><a href=""></a></div>
                 <div><a href=""></a></div>
             </div>
+        <?php
+    }
+    public function displayFooter(){
+        ?>
+        </body>
         <?php
     }
 }
